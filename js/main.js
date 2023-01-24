ca4e8600cd46e2a55aa64450969cd5cbad8222ec
CarloColletti~ <carlocolletti5@gmail.com>
2023-01-24 15:49:50 +0100

    reso meglio codice

--- js/main.js
+++ js/main.js
@@ -7,33 +7,37 @@ console.log("La distanza da percorrere è: ", km);
 const età = prompt("Quanti anni hai?")
 console.log("L'età del passeggero è: ", età);
 
+// costo biglietto a km 
+const tariffa = 0.21;
+costo = tariffa * km;
+
 // l'età è compresa tra i 18 e i 64 anni?
 if (età >= 18 && età <=64) {
-    let percorso = km;
-    let tariffa = 0.21;
-    let costo = tariffa * percorso;
     console.log("Il costo del biglietto è:  ", costo.toFixed(2));
     document.getElementById("c-biglietto").innerHTML = (costo.toFixed(2));
 }
 // l'età è inferiore hai 18 anni?
 else if (età >= 65) {
-    let percorso = km;
-    let tariffa = 0.21;
-    let costo = tariffa * percorso;
+    // prezzo senza sconto 
     console.log("Il costo del biglietto senza sconto è:  ", costo.toFixed(2));
+
+    // eseguo uno sconto del 40% 
     let sconto = (costo / 100) * 40;
     let costo_sc = costo - sconto;
+    // costo finale del biglietto
     console.log("Il costo del biglietto è:  ", costo_sc.toFixed(2));
     document.getElementById("c-biglietto").innerHTML = (costo_sc.toFixed(2));
 }
 // l'età è inferiore hai 18 anni?
 else {
-    let percorso = km;
-    let tariffa = 0.21;
-    let costo = tariffa * percorso;
+    // prezzo senza sconto 
     console.log("Il costo del biglietto senza sconto è:  ", costo.toFixed(2));
+
+    // eseguo uno sconto del 20% 
     let sconto = (costo / 100) * 20;
     let costo_sc = costo - sconto;
+
+    // costo finale del biglietto
     console.log("Il costo del biglietto è:  ", costo_sc.toFixed(2));
     document.getElementById("c-biglietto").innerHTML = (costo_sc.toFixed(2));
 }
